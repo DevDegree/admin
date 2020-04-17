f7cb674f2e169a8dbcd3b09424424f594674de59
Derek Ledoux <derek.ledoux@shopify.com>
2020-04-17 08:44:01 -0400

    setting up workshop project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useCallback, useState} from 'react';
+import '@shopify/polaris/styles.css';
+import {Frame, TopBar, Navigation} from '@shopify/polaris';
+import Home from './components/Home';
+import Orders from './components/Orders';
+import Products from './components/Products'
 
 function App() {
+  const [menuState, setMenuState] = useState('home');
+  const setPageMarkup = useCallback(
+    (value) => setMenuState(value),
+    [],
+  );
+
+  const userMenuMarkup = (
+    <TopBar.UserMenu
+      initials='🦄'
+      name='Dev Degree'
+      detail="2020 Cohort"
+    />
+  );
+
+  const searchFieldMarkup = (
+    <TopBar.SearchField
+      placeholder="Search"
+      onChange={()=>{}}
+      showFocusBorder
+    />
+  );
+
+  const topBarMarkup = (
+    <TopBar
+      showNavigationToggle
+      userMenu={userMenuMarkup}
+      searchField={searchFieldMarkup}
+    />
+  );
+
+  const navigationMarkup = (
+    <Navigation location="/">
+      <Navigation.Section
+        separator
+        items={[
+          {
+            label: 'Home',
+            onClick: () => setPageMarkup('home'),
+          },
+          {
+            label: 'Orders',
+            onClick: () => setPageMarkup('orders'),
+          },
+          {
+            label: 'Products',
+            onClick: () => setPageMarkup('products'),
+          },
+        ]}
+      />
+    </Navigation>
+  );
+
+  const getCurrentMarkup = () => {
+    switch(menuState){
+      case 'orders':
+        return (<Orders />);
+      case 'products':
+          return (<Products />);
+      default:
+        return (<Home />)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Frame
+      topBar={topBarMarkup}
+      navigation={navigationMarkup}
+    >
+      {getCurrentMarkup()}
+    </Frame>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import enTranslations from '@shopify/polaris/locales/en.json';
+import {AppProvider} from '@shopify/polaris';
+
+const theme = {
+  colors: {
+    topBar: {
+      background: '#1C2260',
+    }
+  }
+};
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <AppProvider theme={theme} i18n={enTranslations}>
     <App />
-  </React.StrictMode>,
+  </AppProvider>,
   document.getElementById('root')
 );
 
